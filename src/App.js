46a68f543579ebb2bdb2189f225da69fb46269e9
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-07-01 09:12:08 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
+import {
+  
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from './components/Home';
+import Todo from './components/Todo';
+import Calender from './components/Calender';
+import Footer from './components/Footer';
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/todo' element={<Todo />}></Route>
+          <Route path='/calendar' element={<Calender />}></Route>
+          
+
+        </Routes>
+        <Footer />
+   </div>
+  )
 }
 
 export default App;
