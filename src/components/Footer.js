ad180c4286374179c3f61027125b9ee39664dc8f
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-07-02 09:18:13 +0600

    submit on enter key edited

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,11 @@ const Footer = () => {
     const currentYear = (new Date().getFullYear())
 const yearTxt = currentYear === 2022 ? "2022" : "2022 - "+currentYear
     return (
-        <div className='container-fluid bg-dark'>
+        <div className='container-fluid bg-primary'>
             <div class="row">
-      <div class="col-md-12 text-center p-5 text-white">
+      <div class="col-md-12 text-center p-4 text-white">
          
-          <h1  class="headline text-warning pt-5">To-do App</h1>
+          <h1  class="headline text-warning pt-2">To-do App</h1>
           
          
           
